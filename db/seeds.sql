7c8a5f1ce257bfa8e270e0b655bb48d84481d5ce
James <jwtankersley10@gmail.com>
2020-03-17 18:40:49 -0500

    added schema and seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Impossible Meat Burger",false), ("Cheese Burger",false), ("Veggie Burger",false), ("Big Mac", false), ("Whopper", false);
