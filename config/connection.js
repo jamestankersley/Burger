b8d32340d1f8ba044b0242f041a2a555c16bbe15
James <jwtankersley10@gmail.com>
2020-03-27 19:33:05 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {
   connection = mysql.createConnection({
-    host: "s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "hqmjl4jtm5zuyop5",
-    password: "hxv0j56l1c1d1n2e",
-    database: "gqv8fq0w5sdouwan"
+    host: "localhost",
+    user: "root",
+    password: "baller9",
+    database: "burgers_db"
   })
 };
 
