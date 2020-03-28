7ca3eadaf65a4a2136495367888be755c9a1375d
James <jwtankersley10@gmail.com>
2020-03-27 19:07:55 -0500

    final push

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-
-var app = express();
 var PORT = process.env.PORT || 3000;
+var app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgersController.js");
+
 app.use(routes);
 
 app.listen(PORT, function() {
