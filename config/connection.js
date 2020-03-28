fbbee0c3381e6e26f89c901f92a010c6ec7aaaeb
James <jwtankersley10@gmail.com>
2020-03-27 19:45:53 -0500

    changed connection to Jaws

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "baller9",
-    database: "burgers_db"
+    host: "izm96dhhnwr2ieg0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "db52msbol953s8qy",
+    password: "l4vq8fg7cmegloe2",
+    port: 3306,
+    database: "c0pci31wnd53autd"
   })
 };
 
