fd2120b2e211072b79334866584bd5113e108ded
James <jwtankersley10@gmail.com>
2020-03-27 19:00:29 -0500

    final controller push

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,52 +1,42 @@
 var express = require("express");
-var burger = require("../models/burger");
-
 var router = express.Router();
+var burgerModel = require("../models/burgers.js");
 
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-    var hdbrsObj = {
-      burgers: data
-    };
-    console.log(hdbrsObj);
-    res.render("index", hdbrsObj);
-  });
-
-  router.post("/api/burgers", function(req, res) {
-    burger.insertOne(
-      ["burger_name", "devoured"],
-      [req.body.burger_name, req.body.devoured],
-      function(result) {
-        
-        res.json({ id: result.insertId });
-      }
-    );
-  });
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(
-      result
-    ) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+    burgerModel.selectAll(function(data) {
+        var allObject = {
+            burgers: data
+        };
+        console.log(allObject);
+        res.render("index", allObject);
     });
-  });
-  router.delete("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
-
-    burger.deleteOne(condition, function(result) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
 });
+
+router.post("/api/burgers", function(req, res) {
+    burgerModel.createOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], 
+        function(result) {
+            res.json({id: result.insertID});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    console.log(req.body);
+    console.log(req.body.devoured);
+
+    burgerModel.updateOne(
+        {devoured: req.body.devoured}, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+    
 module.exports = router;
